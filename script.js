02744e01b2d7cd3ab4cbf4d5aeacf5392a0e07af
KosisochukwuD <kosiokechukwu9@gmail.com>
2024-01-06 09:03:13 +0100

    added icons and few other codes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function menubar(){
+   var when=document.getElementById('menubar')
+  if(when.style.display==='block'){
+    document.getElementById('menubar').style.display='none'
+  }
+  else{
     document.getElementById('menubar').style.display='block'
+  }
 }
