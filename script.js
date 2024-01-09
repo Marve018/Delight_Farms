d31ba46efc4be1f9ec86b442347f5b063a8cc816
KosisochukwuD <kosiokechukwu9@gmail.com>
2024-01-09 13:21:32 +0100

    login page

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ function menubar(){
   else{
     document.getElementById('menubar').style.display='block'
   }
+  
+}
+function button(){
+   var email = document.getElementById('email').value
+   var password = document.getElementById('password')
+   var messageElement = document.getElementById('message');
+   messageElement.textContent = `User ${email} signed up successfully!`;
+  document.getElementById('image').style.display='block'
+   setTimeout(function () {
+    window.location.href = "index.html"; // Replace with your actual index page URL
+}, 1500);
+
 }
