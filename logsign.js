4dd6a97507de8a31bb89b5b34a00fa7141fbd260
KosisochukwuD <kosiokechukwu9@gmail.com>
2024-01-10 00:56:40 +0100

    functionality

---
+++ logsign.js
@@ -0,0 +1,35 @@
+ 
+  
+
+function button(){
+   var email = document.getElementById('email').value
+   var pass = document.getElementById('password')
+   var messageElement = document.getElementById('message');
+   messageElement.textContent = `User ${email} signed up successfully!`;
+   document.getElementById('image').style.display='block'
+    setTimeout(function () {
+     window.location.href = "index.html"; // Replace with your actual index page URL
+ }, 2000);
+ 
+} 
+function login(){
+    Window.location.href = "login.html"
+}
+function validateForm() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm_password").value;
+
+    if (password !== confirmPassword) {
+        document.getElementById('error').innerHTML='passwords dont match'
+        return false;
+    }
+    document.getElementById('image').style.display='block'
+    setTimeout(function () {
+     window.location.href = "index.html"; // Replace with your actual index page URL
+ }, 2000);
+   
+    // Additional validation logic can be added here
+
+    return true;
+}
+
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function menubar(){
    var when=document.getElementById('menubar')
   if(when.style.display==='block'){
@@ -8,14 +9,8 @@ function menubar(){
   }
   
 }
-function button(){
-   var email = document.getElementById('email').value
-   var password = document.getElementById('password')
-   var messageElement = document.getElementById('message');
-   messageElement.textContent = `User ${email} signed up successfully!`;
-  document.getElementById('image').style.display='block'
-   setTimeout(function () {
-    window.location.href = "index.html"; // Replace with your actual index page URL
-}, 1500);
 
+function closenav(){
+  var wen=document.getElementById('menubar')
+  wen.style.display='none'
 }
